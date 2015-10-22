1ef6e4ad3de8ade2f40ed8a3d0384921c2fbcb12
John Santerre <john.santerre@gmail.com>
2015-10-22 10:17:13 -0500

    Update my_utils.py

--- my_utils.py
+++ my_utils.py
@@ -2,6 +2,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import numpy as np
 from sklearn.cross_validation import StratifiedKFold
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
+import csv
+
 
 low_ranges = [10,20,30,40,50,60,70,80,90,100]    
 mid_ranges = [100,150,200,250,300,350,400,450,500]
