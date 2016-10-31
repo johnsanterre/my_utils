54cdb08ed126fc33bf22ea8cecd52c9267944720
John Santerre <john@johnsanterre.com>
2016-10-31 14:07:08 -0500

    new update

--- my_utils.py
+++ my_utils.py
@@ -1,8 +1,10 @@
 from __future__ import division
 import numpy as np
-import matplotlib.pyplot as plt
-import csv
+#import matplotlib.pyplot as plt
+#matplotlib.use('qt4agg')
+#plt.style.use('ggplot')
 
+import csv
 
 from itertools import product
 from collections import Counter
@@ -14,9 +16,13 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.metrics import accuracy_score, auc, f1_score, precision_score, recall_score, roc_curve
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 from scipy.stats import rankdata
 import difflib
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+import networkx as nx
+
 
 #supress Scientific Notation
 np.set_printoptions(precision=2)
@@ -40,16 +46,28 @@ def convert_real_value_matrix_to_row_ranking_matrix(M):
 
 def percent_nonzero(M):
   return  len(np.nonzero(M)[0])/float(np.multiply(*M.shape))
-  
+
 #!import code; code.interact(local=vars())
 
 def range_datetime(base, numdays):
     return [base - datetime.timedelta(days=x) for x in range(0, numdays)]
-    
+
 def save_session(floc='history.txt'):
     #overrights pevious history
     import readline
     readline.write_history_file(floc)
+
+def get_tree_assoicated_with_feature(data, featureid):
+  return np.nonzero(data[featureid])[0]
+
+def get_postion_of_feature_in_trees(data, featureid):
+  ret = []
+  for x in get_tree_assoicated_with_feature(data,featureid):
+    all_features =  np.nonzero(data[:,x])[0]
+    vals = data[all_features, x]
+    ranks = reverse_rank(vals)
+    ret.append(ranks[np.where(all_features==featureid)])
+  return ret
 
 def convert_ordered_ranks_HR(sss):
     return [(x,rank_agg_idx[y]) for x, y in sss]
@@ -64,7 +82,7 @@ def setup_for_HR(clf_on_mM, M, labels):
     return clf_on_mM, mM, tM
 
 def rank_index_value_tuples(s):
-    rank_index= sorted(zip((len(s)+1-rankdata(s)), range(len(s))), key=lambda x: x[0]) 
+    rank_index= sorted(zip((len(s)+1-rankdata(s)), range(len(s))), key=lambda x: x[0])
     ret = {}
     for idx,x in enumerate(rank_index):
       ret[x[0]] ={'loc':x[1], 'val':s[x[1]]}
@@ -84,7 +102,7 @@ def get_Y_weight(tM):
             Y[zero,one] += row[combo[0]]-row[combo[1]]
             weight[zero,one] +=1
         print i
-    return Y, weight    
+    return Y, weight
 
 def hodgeRank(Y, weight):
     W = weight
@@ -107,7 +125,7 @@ def get_top_features(clf, top_n):
     for v in val:
         if v != 0:
             index.append(np.where(clf.feature_importances_==v)[0])
-    
+
     idx = [x[0][0] for x in index] #test this
     return val, idx
 
@@ -117,7 +135,7 @@ def matrix_to_binary_indicator_matrix(M):
     return  (M>0).astype(int)
 
 def remove_duplicate_columns(M):
-    #removes identical columns to shirnk the size of a matrix. 
+    #removes identical columns to shirnk the size of a matrix.
     #example:
     return np.array(list({tuple(M[:,x]) for x in range(len(M[0]))})).T
 
@@ -142,9 +160,9 @@ def distance_in_sequences(a,b):
     return seq.ratio()
 
 def feature_matrix_from_clf(clf):
-    #example:    
+    #example:
     M = []
-    for est in clf.estimators_: 
+    for est in clf.estimators_:
         M.append(est.tree_.compute_feature_importances())
     return np.array(M)
 
@@ -153,18 +171,18 @@ def pickle_this_datetime(obj, f_loc):
   import pickle
   end = str(datetime.datetime.now())
   f_loc = f_loc + '_' + end
-  with open(f_loc, 'wb') as handle:  
+  with open(f_loc, 'wb') as handle:
       pickle.dump(obj, handle)
   return
-  
+
 def pickle_this(obj, f_loc='cucumber.pickle'):
     import pickle
-    with open(f_loc, 'wb') as handle:  
+    with open(f_loc, 'wb') as handle:
         pickle.dump(obj, handle)
     return
 
 def unpickle(f_loc='cucumber.pickle'):
-    import pickle    
+    import pickle
     return pickle.load( open( f_loc,'rb'))
 
 def run_bash(bashCommand):
@@ -177,7 +195,7 @@ def isfile(f_loc):
 
 def import_file_from_url(f_loc='https://raw.githubusercontent.com/johnsanterre/my_utils/master/my_utils.py'):
     #snake eating itself
-    import urllib2; 
+    import urllib2;
     tmp= urllib2.urlopen(f_loc);
     exec(tmp.read())
 
@@ -206,7 +224,7 @@ def read_fasta(floc):
             reader = csv.reader(f,delimiter='\t')
             data = list(reader)
     all_fasta={}
-    for row in data:    
+    for row in data:
         if row[0][0]=='>':
             peg, func = row[0][1:].split(' ',1)
             all_fasta[peg]={'function':func}
@@ -223,15 +241,15 @@ def open_a_fangFang(loc, delim):
     inv_index = {}
     labels = data[0][1:]
     data = data[1:]
-    
+
     for idx, x in enumerate(data):
         index[x[0]]=idx
         inv_index[idx]=x[0]
-        
+
     data = np.transpose(np.array([x[1:] for x in data], dtype=(np.int16)))
-    
+
     return data, labels, index, inv_index
-    
+
 def read_dataset(fname):
     #FF
     f = open(fname)
@@ -249,24 +267,24 @@ def inverse_dict(aDict):
 def rand_set(the_list, n_samples):
     import random
     random.shuffle(the_list)
-    return the_list[0:n_samples]    
+    return the_list[0:n_samples]
 
 
-    
-  
+
+
 
 
 
 #sklearn helpers
 def run_clf(clf, skf, M, labels):
     acc= []
-    for tr,tst in skf: 
+    for tr,tst in skf:
         clf.fit(M[tr],labels[tr])
         acc.append(accuracy_score(clf.predict(M[tst]),labels[tst]))
     return acc
 
-    
-#std format    
+
+#std format
 def get_top_features_and_values(clf, n_top):
     return ( [ (x,clf.feature_importances_[x]) for
             x in clf.feature_importances_.argsort()[-n_top:][::-1]])
@@ -280,16 +298,16 @@ def top_important_features(clf, feature_names, num_features=20):
     top = sorted(features, key=lambda f:f[0], reverse=True)[:num_features]
     return top
 
-#exp helpers    
+#exp helpers
 def avg_acc_on_subset(clf,skf,M,labels,n):
     sub = rand_set(range(M.shape[0]),n)
     skf = StratifiedKFold(labels[sub], n_folds=5)
-    return np.average(run_clf(clf,skf, M[sub,:],labels[sub]))    
+    return np.average(run_clf(clf,skf, M[sub,:],labels[sub]))
 
 def avg_avg_acc_on_subset(clf, skf, M, labels, n, runs):
     acc = []
     for x in range(runs):
-        acc.append(avg_acc_on_subset(clf,skf, M,labels,n)) 
+        acc.append(avg_acc_on_subset(clf,skf, M,labels,n))
     return np.average(acc), np.std(acc)
 
 def avg_avg_acc_on_balanced_subset(clf,M,labels,n,runs):
@@ -311,7 +329,7 @@ def run_subset_exp(clf, M, labels,ranges):
         tmp_results.append(avg_avg_acc_on_balanced_subset(clf,M,labels,r,10))
         results.append(tmp_results)
     return results
- 
+
 # go thru this
 def get_identicle_value_columns(M):
     col_ids = []
@@ -324,10 +342,10 @@ def agreement_fit(data,inv_index,type_one, type_two,count_Yes_No):
     one = [col_idx for col_idx in range(data.shape[1]) if np.count_nonzero(data[type_one, col_idx])==count_Yes_No[0]]
     two= [col_idx for col_idx in one if np.count_nonzero(data[type_two,col_idx])==count_Yes_No[1]]
     return [inv_index[x] for x in two]
-    
+
 def RES_id_from_SUS_id(SUS_ids, total_num_samples):
     return [x for x in range(total_num_samples) if x not in SUS_ids]
-    
+
 rf_sweep = {'clf': RandomForestClassifier,
              'n_estimators': [10,30,50],
              'max_features': ['sqrt','log2'],
